bf00ddfab794aab262a085baa3fefe63677fe4c2
Saliou SARR <m2sproduction@hotmail.fr>
2021-01-24 23:09:28 +0100

    #Add: Handle user token expired

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,6 +7,9 @@ module.exports =  (req, res, next) => {
         jwt.verify(token, process.env.SECRET_AUTH_TOKEN);
         next();       
       } catch(error) {
+        if(error.name === 'TokenExpiredError') {
+          throw new ApiError("User token has expired. You'll be disconnected.", 498)
+        }
         throw new ApiError("User not authorized to connect to this API", 401)
       }
     };
