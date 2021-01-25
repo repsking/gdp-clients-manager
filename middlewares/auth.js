6916080cdc52183edd97e10af19e77f581115bfc
Saliou SARR <m2sproduction@hotmail.fr>
2021-01-25 22:20:23 +0100

    #Put: Uuid of user on req object

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,8 +4,11 @@ const ApiError = require('../Errors/ApiError');
 module.exports =  (req, res, next) => {
     try {
         const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
-        jwt.verify(token, process.env.SECRET_AUTH_TOKEN);
-        next();       
+        const decodedToken = jwt.verify(token, process.env.SECRET_AUTH_TOKEN);
+        const userId = decodedToken.userId;
+        if (req.headers.uuid && req.headers.uuid !== userId) throw ApiError('Invalid user ID',498);
+        req.currentUserId = userId;
+        next()
       } catch(error) {
         if(error.name === 'TokenExpiredError') {
           throw new ApiError("User token has expired. You'll be disconnected.", 498)
