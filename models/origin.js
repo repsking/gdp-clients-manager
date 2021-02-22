b2f048615e875b80668b82e523040ec8c2760582
Saliou SARR <m2sproduction@hotmail.fr>
2021-02-22 10:16:19 +0100

    Add origine theme Color

--- models/origin.js
+++ models/origin.js
@@ -1,11 +1,15 @@
-const {Schema, model} = require('mongoose');
-const {requiredString} = require('./utils/customSchemaType')
+const { Schema, model } = require('mongoose');
+const { requiredString } = require('./utils/customSchemaType')
 
 const originSchemas = Schema({
-    name: {...requiredString, unique: true},
+    name: {...requiredString, unique: true },
     url: {
-      ...requiredString,
+        ...requiredString,
         unique: true
+    },
+    color: {
+        type: String,
+        default: "#bbb"
     },
     keywords: [String]
 })
@@ -14,11 +18,11 @@ originSchemas.statics.findIdByKeyword = async function(word) {
     const $regex = new RegExp(word, 'i');
     const res = await this.findOne({ keywords: { $regex } });
     return res && res._id;
-  };
+};
 
-  originSchemas.statics.findByKeyword = async function(word) {
+originSchemas.statics.findByKeyword = async function(word) {
     const $regex = new RegExp(word, 'i');
     const res = await this.findOne({ keywords: { $regex } });
     return res
-  };
+};
 module.exports = model('Origin', originSchemas);
