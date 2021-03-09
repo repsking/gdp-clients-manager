812c2442963421ddf1abc95f742a84f95bb603e4
Saliou SARR <m2sproduction@hotmail.fr>
2021-03-09 20:53:31 +0100

    New test

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,6 @@ router.get('/contributors', authUser, authRole(ROLES.contributor), getContributo
 router.post('/newuser', authUser, authRole(ROLES.admin), newUser);
 router.put('/changePassword', authUser, changePassword);
 router.put('/updateUser', authUser, updateUserInfo);
-router.get('/test', (req,res) => res.json('test') );
+router.get('/test', (req,res) => res.json('Again') );
 
 module.exports = crud(User, { router, needAuth: true, role: ROLES.admin });
