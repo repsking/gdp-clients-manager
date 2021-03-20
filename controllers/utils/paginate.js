e1d1a1ca19b7edd3fe36e7592b90516e72233e63
Saliou SARR <m2sproduction@hotmail.fr>
2021-03-20 18:11:13 +0100

    Fix paginated demands controller

--- controllers/utils/paginate.js
+++ controllers/utils/paginate.js
@@ -1,16 +1,15 @@
 const { cleanUndefined } = require('../../utils')
 
 
-const serializeRelation = async(filter) => {
+const serializeRelation = async (filter) => {
     const keys = Object.keys(filter);
     const final = {};
-    console.log({filter})
-    for(const key of keys ) {
-        if(!keys[key]) continue;
-        const {populate, Model, filterValue, field} = keys[key];
-        if(Model && populate && filterValue, field){
-            const result = await Model.find(filterValue,{_id: true});
-            if (result && result._id) final[key] = {[field]: result._id};
+    for (const key of keys) {
+        if (!keys[key]) continue;
+        const { populate, Model, filterValue, field } = keys[key];
+        if (Model && populate && filterValue, field) {
+            const result = await Model.find(filterValue, { _id: true });
+            if (result && result._id) final[key] = { [field]: result._id };
         }
     }
     return final;
@@ -22,17 +21,15 @@ const serializeRelation = async(filter) => {
  * @param {by: String, direction: String} sort 
  * @param {page: Number, limit: Number} pagination 
  */
-exports.paginatedController = async (Model, filter = {}, {by ='id', direction = 'asc'}, {page = 1 , limit = 10}) => {
+exports.paginatedController = async (Model, filter = {}, { by = 'id', direction = 'asc' }, { page = 1, limit = 10 }) => {
     const currentPage = parseInt(page);
     const itemPerPage = parseInt(limit);
-    let localFilter = await serializeRelation(filter);
-    console.log({localFilter});
-    
-    if(localFilter.search && Model.fieldsSearchFilter) {
-        localFilter = {...localFilter, $or: Model.fieldsSearchFilter(filter.search)}
+    let localFilter = { ...filter };
+
+    if (localFilter.search && Model.fieldsSearchFilter) {
+        localFilter = { ...localFilter, $or: Model.fieldsSearchFilter(filter.search) }
         delete localFilter.search;
     }
-
     localFilter = cleanUndefined(localFilter);
     const startIndex = (currentPage - 1) * itemPerPage;
     const endIndex = currentPage * itemPerPage;
@@ -42,7 +39,6 @@ exports.paginatedController = async (Model, filter = {}, {by ='id', direction = 
     const meta = {};
     if (endIndex < totalItems) meta.next = currentPage + 1;
     if (startIndex > 0) meta.previous = currentPage - 1;
-    console.log({localFilter});
     const results = await Model.find(localFilter).limit(itemPerPage).skip(startIndex).sort({ [by]: direction }).exec();
     return {
         results,
