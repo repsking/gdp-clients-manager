8e7f33f8a257c212bebe685f189c91afcfacede0
Saliou SARR <m2sproduction@hotmail.fr>
2021-03-20 18:22:39 +0100

    Clean dummy demands and lint

--- controllers/demands/index.js
+++ controllers/demands/index.js
@@ -12,13 +12,13 @@ const { sendResponseMail } = require('./emailsManager');
 
 
 
-const createDemand = async(demand) => {
+const createDemand = async (demand) => {
     const [originId = null, statusId] = await Promise.all([
         Origin.findIdByKeyword(demand.origin),
         Status.findIdByCode("new"),
     ]);
     if (!statusId) throw ApiError("status_not_defined", 500);
-    const doc = new Demands({...demand, origin: originId, handler: { status: statusId } });
+    const doc = new Demands({ ...demand, origin: originId, handler: { status: statusId } });
     const res = await doc.save();
     sendResponseMail(res);
     return res;
@@ -32,11 +32,11 @@ exports.createCommonDemand = controller(({ body }) => {
 }, ACTION.CREATE);
 
 exports.createBeContactedDemand = controller(({ body }) => {
-    return createDemand(serializeDemand({...body, message: "Auto: 'Ce prospect désire être recontacté.'" }));
+    return createDemand(serializeDemand({ ...body, message: "Auto: 'Ce prospect désire être recontacté.'" }));
 }, ACTION.CREATE);
 
 exports.createProgramDemand = controller(({ body }) => {
-    return createDemand(serializeProgrammeDemand({...body }));
+    return createDemand(serializeProgrammeDemand({ ...body }));
 }, ACTION.CREATE);
 
 exports.addComment = controller(async ({ user, params, body }) => {
@@ -48,29 +48,31 @@ exports.removeComment = controller(({ params }) => {
     return Demands.updateOne({ _id: params.id }, { comment: null });
 }, ACTION.INFORM);
 
-exports.assignToUser = controller(async({ body: { userId }, params }) => {
-    const user = await User.findById(userId, {nom: true, prenom: true, id: true});
+exports.assignToUser = controller(async ({ body: { userId }, params }) => {
+    const user = await User.findById(userId, { nom: true, prenom: true, id: true });
     if (!user) throw new ApiError("Selected user doesn't exist", 400);
-    const status = await Status.findOne({ code: { $regex: 'handled' } }); 
+    const status = await Status.findOne({ code: { $regex: 'handled' } });
     await Demands.updateOne({ _id: params.id }, { handler: { userId, status: status.id } });
     return { userId: user, status }
 }, ACTION.RESULT);
 
-exports.updateStatus = controller(async({ body: { statusId }, params }) => {
+exports.updateStatus = controller(async ({ body: { statusId }, params }) => {
     const statusExist = await Status.exists({ _id: statusId });
     if (!statusExist) throw new ApiError("This status doesn't exist", 400);
     return Demands.updateOne({ _id: params.id }, { handler: { status: statusId } });
 }, ACTION.INFORM);
 
-exports.generateContact = controller(async({ body: { comment }, params }) => {
-    const demand = await Demands.findById(params._id);
-    if(!demand) throw new ApiError("This demands doesn't exist");
+exports.cleanDummy = controller(() => Demands.deleteMany({ 'user.email': 'edart@gmail.com'}), ACTION.INFORM);
 
-    const user = new Contact({...demand.user, comment});
+exports.generateContact = controller(async ({ body: { comment }, params }) => {
+    const demand = await Demands.findById(params._id);
+    if (!demand) throw new ApiError("This demands doesn't exist");
+
+    const user = new Contact({ ...demand.user, comment });
     const userError = user.validateSync();
     if (userError) throw new ApiError("The user in the contact demand seems to have some problems");
-    
-    await Promise.all([ user.save(), Demands.updateOne({ _id: params.id }, { user })]);
+
+    await Promise.all([user.save(), Demands.updateOne({ _id: params.id }, { user })]);
     return user;
 }, ACTION.RESULT);
 
--- routes/demands.js
+++ routes/demands.js
@@ -1,20 +1,21 @@
 const Demands = require('../models/demands')
-const {authUser, authRole} = require('../middlewares/auth');
+const { authUser, authRole } = require('../middlewares/auth');
 const { ROLES } = require("../config/roles")
-const {addComment, assignToUser, removeComment, createProgramDemand, createCommonDemand, createBeContactedDemand, paginatedList, importDemands, generateContact} = require('../controllers/demands');
+const { addComment, assignToUser, removeComment, createProgramDemand, createCommonDemand, createBeContactedDemand, paginatedList, importDemands, generateContact, cleanDummy } = require('../controllers/demands');
 const ehpad = require('../controllers/ehpad');
 const { Router } = require('express');
 const router = Router();
- 
+
 router.get('/paginated', authUser, paginatedList)
 router.post('/common', createCommonDemand)
 router.post('/programme', createProgramDemand)
 router.post('/beContacted', createBeContactedDemand)
-router.post('/addComment/:id',authUser, addComment)
-router.post('/removeComment/:id',authUser, removeComment)
+router.post('/addComment/:id', authUser, addComment)
+router.post('/removeComment/:id', authUser, removeComment)
+router.post('/cleanDummy', authUser, authRole(ROLES.reps), cleanDummy)
 router.post('/assignTo/:id', authUser, authRole(ROLES.admin), assignToUser)
 router.post('/import', authUser, authRole(ROLES.reps), importDemands)
 router.post('/generateContact/:id', authUser, generateContact)
 router.post('/ehpad', ehpad)
 
-module.exports = require('./utils/crud')(Demands, {router, noCreate: true, needAuth: true, role: ROLES.contributor });
+module.exports = require('./utils/crud')(Demands, { router, noCreate: true, needAuth: true, role: ROLES.contributor });
